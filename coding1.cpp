cec0f227abd6f398589674cd4364bed258e5c12d
Krrish Sinha <142652315+krrishsinha20@users.noreply.github.com>
2023-10-10 22:55:20 +0530

    Add files via upload

---
+++ coding1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;   
+int main(){
+    float x;
+    cout<<"enter number : "<<endl;
+    cin>>x;
+    cout<<"float value is "<<x<<endl;
+    cout<<"int value is "<<int(x)<<endl;
+    return 0;
+}
